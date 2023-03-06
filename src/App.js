c72122d893a7f0fb9f2d7fdca5eea4de83c27078
Syed_Sulaiman <sshq2k17@gmail.com>
2023-03-06 19:49:02 +0530

    CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LandingPage from './components/LandingPage';
+import HomePage from './components/HomePage';
+import ForgetPasswordPage from './components/ForgetPasswordPage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <div>
+                <Routes>
+                    <Route exact path="/" element={ <LandingPage /> } />
+                    <Route exact path="/login" element={ <LoginPage /> } />
+                    <Route exact path="/register" element={ <RegisterPage /> } />
+                    <Route exact path="/forget-password" element={ <ForgetPasswordPage /> } />
+                    <Route exact path="/home" element={ <HomePage /> } />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
   );
 }
 
 export default App;
+
+const Footer = () => {
+  return (
+      <p className="text-center" style={ FooterStyle }>Designed & coded by <a href="https://izemspot.netlify.com" target="_blank" rel="noopener noreferrer">IZEMSPOT</a></p>
+  )
+}
+
+const FooterStyle = {
+  background: "#222",
+  fontSize: ".8rem",
+  color: "#fff",
+  position: "absolute",
+  bottom: 0,
+  padding: "1rem",
+  margin: 0,
+  width: "100%",
+  opacity: ".5"
+}
